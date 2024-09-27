8df2feced1f7bda244698d336ff3eb96de4a9990
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-09-27 21:02:35 +0500

    3.cs

--- Home_work/Module_3/KISS/3.cs
+++ Home_work/Module_3/KISS/3.cs
@@ -16,3 +16,29 @@ public void ProcessNumbers(int[] numbers)
         }
     }
 }*/
+
+/*мой ответ*/
+using System;
+public class ProcessNumbers{
+    public static void Main()
+    {
+        Console.WriteLine("write your numbers thru 'space': ");
+        string input = Console.ReadLine();
+        string[] inputNumbers = input.Split(' '); /*для ввода так как в начальном коде используется "foreach (var number in numbers)"*/
+        foreach (var numbers in inputNumbers)
+        {
+            if (int.TryParse(numbers, out int number) && number > 0) /*тут int.TryParse преврашяет наш текстовый ввод в int а numbers, 
+                out int number присваевает int к number*/
+            {
+                Console.WriteLine(numbers + " > 0");
+            }
+        }
+        /*string input = Console.ReadLine();
+        int numbers = int.Parse(input);
+        Console.WriteLine($"{numbers} > 0 = {numbers > 0}");*/
+    }
+}
+/*я не добавил if (numbers != null) и if (numbers.Length > 0), так как без ввода прога не выполнится а если ввести 
+что угодно (если ввести негативные числа то программа заработает) то оно полюбому будет больше или меньше 0
+так как в оригинальном коде не было никакой проверки что ввод это не другой символ или что ввод может быть меньше 0 я этого не стал добавлять
+а то что я записал в комменты в коде это метод еще проще но оно работает только с одним вводом и выводит boolean*/
