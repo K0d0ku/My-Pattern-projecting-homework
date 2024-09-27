c9b8edd093eea7956c763c4d4c0aea83c9da0a09
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-09-27 20:24:08 +0500

    Create 3.cs

---
+++ Home_work/Module_3/KISS/3.cs
@@ -0,0 +1,18 @@
+/*Произведите корректную (правильную) по вашему мнению реализацию с применением принципа KISS:
+Избегание ненужного вложения кода
+public void ProcessNumbers(int[] numbers)
+{
+    if (numbers != null)
+    {
+        if (numbers.Length > 0)
+        {
+            foreach (var number in numbers)
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}*/
