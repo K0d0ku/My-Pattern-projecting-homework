b89fb28f27339075a1aeccf567ea03b66f0c15e6
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-09-27 21:44:07 +0500

    2.cs

---
+++ Lab_work/Module_4/OCP/2.cs
@@ -0,0 +1,35 @@
+/*Произведите корректную (правильную) по вашему мнению реализацию с применением принципа Open-Closed Principle, OCP:
+Рассмотрим пример, в котором класс DiscountCalculator нарушает принцип OCP, поскольку каждый раз при добавлении нового типа скидки нужно изменять существующий код:
+public enum CustomerType
+{
+    Regular,
+    Silver,
+    Gold
+}
+
+public class DiscountCalculator
+{
+    public double CalculateDiscount(CustomerType customerType, double amount)
+    {
+        if (customerType == CustomerType.Regular)
+        {
+            return amount;
+        }
+        else if (customerType == CustomerType.Silver)
+        {
+            return amount * 0.9; // 10% скидка
+        }
+        else if (customerType == CustomerType.Gold)
+        {
+            return amount * 0.8; // 20% скидка
+        }
+        else
+        {
+            throw new ArgumentException("Неизвестный тип клиента");
+        }
+    }
+}
+
+Если потребуется добавить новый тип клиента, например, Platinum, то потребуется модифицировать метод CalculateDiscount, что нарушает принцип OCP.
+Необходимо используя полиморфизм, сделать класс DiscountCalculator открытым для расширения, но закрытым для модификации.
+*/
