4b4cd321df19593d283c7ea4c4582e1cf2d818f4
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-09-28 05:07:25 +0500

    5.cs

---
+++ Home_work/Module_3/KISS/5.cs
@@ -0,0 +1,14 @@
+/*Избегание избыточного использования исключений
+public int Divide(int a, int b)
+{
+    try
+    {
+        return a / b;
+    }
+    catch (DivideByZeroException)
+    {
+        return 0;
+    }
+}*/
+
+/**/
