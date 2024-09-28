8b963e3c1f5c3f3861319e403866affcabba94b3
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-09-28 12:35:41 +0500

    5.cs

--- Lab_work/Module_3/Yagni/5.cs
+++ Lab_work/Module_3/Yagni/5.cs
@@ -1,1 +1,68 @@
-text
+/*Произведите корректную (правильную) по вашему мнению реализацию с применением принципа YAGNI:
+
+Избыточное создание абстракций
+public interface IShape
+{
+    double CalculateArea();
+}
+
+public class Circle : IShape
+{
+    private double _radius;
+
+    public Circle(double radius)
+    {
+        _radius = radius;
+    }
+
+    public double CalculateArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
+
+public class Square : IShape
+{
+    private double _side;
+
+    public Square(double side)
+    {
+        _side = side;
+    }
+
+    public double CalculateArea()
+    {
+        return _side * _side;
+    }
+}*/
+/*мой ответ*/
+using System;
+public class Circle
+{
+    private double _radius;
+
+    public Circle(double radius)
+    {
+        _radius = radius;
+    }
+
+    public double CalculateArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
+public class Square
+{
+    private double _side;
+
+    public Square(double side)
+    {
+        _side = side;
+    }
+
+    public double CalculateArea()
+    {
+        return _side * _side;
+    }
+}
+/*расчет площядей можно сделать без interface*/
