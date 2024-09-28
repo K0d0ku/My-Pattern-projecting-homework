ff34974bd964d813ec7c8145ade376523e69ca6d
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-09-28 12:20:48 +0500

    4.cs

--- Lab_work/Module_3/KISS/4.cs
+++ Lab_work/Module_3/KISS/4.cs
@@ -1,4 +1,4 @@
-Произведите корректную (правильную) по вашему мнению реализацию с применением принципа KISS:
+/*Произведите корректную (правильную) по вашему мнению реализацию с применением принципа KISS:
 Избегание избыточного использования шаблонов проектирования
 public class Singleton
 {
@@ -29,5 +29,32 @@ public class Client
     public void Execute()
     {
         Singleton.Instance.DoSomething();
+    }
+}*/
+
+/*мой ответ*/
+using System;
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Client client = new Client();
+        client.Execute();
+    }
+}
+public class NormalClassXD
+{
+    public NormalClassXD() { }
+    public void DoSomething()
+    {
+        Console.WriteLine("Doing something...");
+    }
+}
+public class Client
+{
+    public void Execute()
+    {
+        NormalClassXD normalClassInstance = new NormalClassXD();
+        normalClassInstance.DoSomething();
     }
 }
