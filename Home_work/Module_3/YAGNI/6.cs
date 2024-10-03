7997b80fa0604b05d18b990b5b4711068aa9cb43
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-10-03 21:52:05 +0500

    6.cs

--- Home_work/Module_3/YAGNI/6.cs
+++ Home_work/Module_3/YAGNI/6.cs
@@ -4,3 +4,36 @@ public class User
     public string Email { get; set; }
     public string Address { get; set; }
 }
+public interface IUserRepos
+{
+    void SaveToDatabase();
+}
+public class DatabaseRepos : IUserRepos
+{
+    public void SaveToDatabase()
+    {
+        // Код для сохранения пользователя в базу данных
+    }
+}
+public interface IEmailService
+{
+    void SendEmail();
+}
+public class EmailService : IEmailService
+{
+    public void SendEmail()
+    {
+        // Код для отправки электронного письма пользователю
+    }
+}
+public interface IPrintAddressLabels
+{
+    void PrintAddressLabel();
+}
+public class PrintAdress : IPrintAddressLabels
+{
+    public void PrintAddressLabel()
+    {
+        // Код для печати адресного ярлыка
+    }
+}
