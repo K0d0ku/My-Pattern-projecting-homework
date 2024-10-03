c5d3b923f2c8e06902fb29dd309f40a851a65636
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-10-03 21:39:01 +0500

    6.cs

--- Home_work/Module_3/YAGNI/6.cs
+++ Home_work/Module_3/YAGNI/6.cs
@@ -1,24 +1,6 @@
-/*Произведите корректную (правильную) по вашему мнению реализацию с применением принципа YAGNI:
-Создание многофункционального класса
-
 public class User
 {
     public string Name { get; set; }
     public string Email { get; set; }
     public string Address { get; set; }
-
-    public void SaveToDatabase()
-    {
-        // Код для сохранения пользователя в базу данных
-    }
-
-    public void SendEmail()
-    {
-        // Код для отправки электронного письма пользователю
-    }
-
-    public void PrintAddressLabel()
-    {
-        // Код для печати адресного ярлыка
-    }
-}*/
+}
