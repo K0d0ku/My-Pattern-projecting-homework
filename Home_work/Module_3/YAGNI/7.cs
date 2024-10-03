0c6f4519ed4cf1651e26f156341f1c899eed5a50
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-10-04 00:03:24 +0500

    7.cs

--- Home_work/Module_3/YAGNI/7.cs
+++ Home_work/Module_3/YAGNI/7.cs
@@ -1,17 +1,7 @@
-Добавление ненужных настроек или конфигураций
 public class FileReader
 {
-    public string ReadFile(string filePath, bool useBuffer = true, int bufferSize = 1024)
-    {
-        // Логика чтения файла с возможностью использования буфера
-        if (useBuffer)
-        {
-            // Чтение с буфером
-        }
-        else
-        {
-            // Чтение без буфера
-        }
+    public string ReadFile()
+    { 
         return "file content";
     }
 }
