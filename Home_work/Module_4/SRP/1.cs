247e50b02df202cce52d8fd8530c9cf98d38c51a
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-10-04 21:09:09 +0500

    1.cs

--- Home_work/Module_4/SRP/1.cs
+++ Home_work/Module_4/SRP/1.cs
@@ -1,5 +1,4 @@
-/*Произведите корректную (правильную) по вашему мнению реализацию с применением принципа Single-Responsibility Principle (SRP):
-В этом примере класс Order отвечает за несколько вещей: хранение данных о заказе, расчет стоимости заказа с учетом скидок, обработку платежа и отправку уведомления пользователю.
+using System;
 public class Order
 {
     public string ProductName { get; set; }
@@ -11,21 +10,20 @@ public class Order
         // Рассчет стоимости с учетом скидок
         return Quantity * Price * 0.9;
     }
-
+}
+public class PaymentProccessing
+{
     public void ProcessPayment(string paymentDetails)
     {
         // Логика обработки платежа
         Console.WriteLine("Payment processed using: " + paymentDetails);
     }
-
+}
+public class NotificationEmail
+{
     public void SendConfirmationEmail(string email)
     {
         // Логика отправки уведомления
         Console.WriteLine("Confirmation email sent to: " + email);
     }
 }
-
-Проблемы:
-•	Класс Order нарушает принцип SRP, так как он отвечает за несколько вещей: расчет цены, обработку платежа и отправку уведомлений. Это усложняет код и делает его менее гибким.
-В этом примере Вам необходимо разделить ответственность между несколькими классами. Класс Order должен отвечает только за хранение данных о заказе. Другие задачи, такие как расчет цены, обработка платежа и отправка уведомлений, должны быть делегированы другим классам.
-*/
