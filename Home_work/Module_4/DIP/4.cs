25c657fdbd041632ed8b867c390e9704e02c3367
Kuro <154610490+K0d0ku@users.noreply.github.com>
2024-10-04 23:31:39 +0500

    4.cs

--- Home_work/Module_4/DIP/4.cs
+++ Home_work/Module_4/DIP/4.cs
@@ -1,1 +1,35 @@
-
+using System;
+public interface INotificationsService
+{
+    public void SendNotification(string message);
+}
+public class EmailSender : INotificationsService
+{
+    public void SendNotification(string message)
+    {
+        Console.WriteLine("Email sent: " + message);
+    }
+}
+public class SmsSender : INotificationsService
+{
+    public void SendNotification(string message)
+    {
+        Console.WriteLine("SMS sent: " + message);
+    }
+}
+public class NotificationService
+{
+    private readonly IEnumerable<INotificationsService> _notificationServices;
+    /*the IEnumerable is for injecting multiple implementations*/
+    public NotificationService(IEnumerable<INotificationsService> notificationServices)
+    {
+        _notificationServices = notificationServices;
+    }
+    public void SendNotification(string message)
+    {
+        foreach (var service in _notificationServices) /*foreach in other hand to iterate over every item*/
+        {
+            service.SendNotification(message);
+        }
+    }
+}
